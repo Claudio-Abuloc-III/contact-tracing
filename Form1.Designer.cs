74363dcc78bcd156a395b40dc5b70c5b2c0e952b
Claudio-Abuloc-III <abulociiiclaudio@gmail.com>
2022-06-10 06:47:14 +0800

    Naming Buttons and TextBoxes for Basic Information

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Contact_Tracing_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Savebutton = new System.Windows.Forms.Button();
+            this.NametextBox = new System.Windows.Forms.TextBox();
+            this.Namebutton = new System.Windows.Forms.Button();
+            this.Birthdaybutton = new System.Windows.Forms.Button();
+            this.BirthdaytextBox = new System.Windows.Forms.TextBox();
+            this.Addressbutton = new System.Windows.Forms.Button();
+            this.AddresstextBox = new System.Windows.Forms.TextBox();
+            this.ContactNobutton = new System.Windows.Forms.Button();
+            this.ContactNotextBox = new System.Windows.Forms.TextBox();
+            this.Occupationbutton = new System.Windows.Forms.Button();
+            this.OccupationtextBox = new System.Windows.Forms.TextBox();
+            this.Emailbutton = new System.Windows.Forms.Button();
+            this.EmailtextBox = new System.Windows.Forms.TextBox();
+            this.Sexbutton = new System.Windows.Forms.Button();
+            this.SextextBox = new System.Windows.Forms.TextBox();
+            this.Agebutton = new System.Windows.Forms.Button();
+            this.AgetextBox = new System.Windows.Forms.TextBox();
+            this.Datebutton = new System.Windows.Forms.Button();
+            this.DatetextBox = new System.Windows.Forms.TextBox();
+            this.Temperaturebutton = new System.Windows.Forms.Button();
+            this.TemperaturetextBox = new System.Windows.Forms.TextBox();
+            this.TimeInbutton = new System.Windows.Forms.Button();
+            this.TimeIntextBox = new System.Windows.Forms.TextBox();
+            this.TimeOutbutton = new System.Windows.Forms.Button();
+            this.TimeOuttextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Savebutton
+            // 
+            this.Savebutton.Location = new System.Drawing.Point(717, 562);
+            this.Savebutton.Name = "Savebutton";
+            this.Savebutton.Size = new System.Drawing.Size(144, 55);
+            this.Savebutton.TabIndex = 0;
+            this.Savebutton.Text = "Save";
+            this.Savebutton.UseVisualStyleBackColor = true;
+            // 
+            // NametextBox
+            // 
+            this.NametextBox.Location = new System.Drawing.Point(106, 35);
+            this.NametextBox.Name = "NametextBox";
+            this.NametextBox.Size = new System.Drawing.Size(384, 22);
+            this.NametextBox.TabIndex = 1;
+            // 
+            // Namebutton
+            // 
+            this.Namebutton.Location = new System.Drawing.Point(12, 31);
+            this.Namebutton.Name = "Namebutton";
+            this.Namebutton.Size = new System.Drawing.Size(88, 31);
+            this.Namebutton.TabIndex = 2;
+            this.Namebutton.Text = "Name";
+            this.Namebutton.UseVisualStyleBackColor = true;
+            // 
+            // Birthdaybutton
+            // 
+            this.Birthdaybutton.Location = new System.Drawing.Point(12, 68);
+            this.Birthdaybutton.Name = "Birthdaybutton";
+            this.Birthdaybutton.Size = new System.Drawing.Size(88, 31);
+            this.Birthdaybutton.TabIndex = 3;
+            this.Birthdaybutton.Text = "Birthday";
+            this.Birthdaybutton.UseVisualStyleBackColor = true;
+            // 
+            // BirthdaytextBox
+            // 
+            this.BirthdaytextBox.Location = new System.Drawing.Point(106, 72);
+            this.BirthdaytextBox.Name = "BirthdaytextBox";
+            this.BirthdaytextBox.Size = new System.Drawing.Size(384, 22);
+            this.BirthdaytextBox.TabIndex = 4;
+            // 
+            // Addressbutton
+            // 
+            this.Addressbutton.Location = new System.Drawing.Point(12, 105);
+            this.Addressbutton.Name = "Addressbutton";
+            this.Addressbutton.Size = new System.Drawing.Size(88, 31);
+            this.Addressbutton.TabIndex = 5;
+            this.Addressbutton.Text = "Address";
+            this.Addressbutton.UseVisualStyleBackColor = true;
+            // 
+            // AddresstextBox
+            // 
+            this.AddresstextBox.Location = new System.Drawing.Point(106, 109);
+            this.AddresstextBox.Name = "AddresstextBox";
+            this.AddresstextBox.Size = new System.Drawing.Size(384, 22);
+            this.AddresstextBox.TabIndex = 6;
+            // 
+            // ContactNobutton
+            // 
+            this.ContactNobutton.Location = new System.Drawing.Point(12, 142);
+            this.ContactNobutton.Name = "ContactNobutton";
+            this.ContactNobutton.Size = new System.Drawing.Size(88, 31);
+            this.ContactNobutton.TabIndex = 7;
+            this.ContactNobutton.Text = "Contact No.";
+            this.ContactNobutton.UseVisualStyleBackColor = true;
+            // 
+            // ContactNotextBox
+            // 
+            this.ContactNotextBox.Location = new System.Drawing.Point(106, 146);
+            this.ContactNotextBox.Name = "ContactNotextBox";
+            this.ContactNotextBox.Size = new System.Drawing.Size(384, 22);
+            this.ContactNotextBox.TabIndex = 8;
+            // 
+            // Occupationbutton
+            // 
+            this.Occupationbutton.Location = new System.Drawing.Point(12, 179);
+            this.Occupationbutton.Name = "Occupationbutton";
+            this.Occupationbutton.Size = new System.Drawing.Size(88, 31);
+            this.Occupationbutton.TabIndex = 9;
+            this.Occupationbutton.Text = "Occupation";
+            this.Occupationbutton.UseVisualStyleBackColor = true;
+            // 
+            // OccupationtextBox
+            // 
+            this.OccupationtextBox.Location = new System.Drawing.Point(106, 183);
+            this.OccupationtextBox.Name = "OccupationtextBox";
+            this.OccupationtextBox.Size = new System.Drawing.Size(384, 22);
+            this.OccupationtextBox.TabIndex = 10;
+            // 
+            // Emailbutton
+            // 
+            this.Emailbutton.Location = new System.Drawing.Point(12, 216);
+            this.Emailbutton.Name = "Emailbutton";
+            this.Emailbutton.Size = new System.Drawing.Size(88, 31);
+            this.Emailbutton.TabIndex = 11;
+            this.Emailbutton.Text = "Email";
+            this.Emailbutton.UseVisualStyleBackColor = true;
+            // 
+            // EmailtextBox
+            // 
+            this.EmailtextBox.Location = new System.Drawing.Point(106, 220);
+            this.EmailtextBox.Name = "EmailtextBox";
+            this.EmailtextBox.Size = new System.Drawing.Size(384, 22);
+            this.EmailtextBox.TabIndex = 12;
+            // 
+            // Sexbutton
+            // 
+            this.Sexbutton.Location = new System.Drawing.Point(496, 31);
+            this.Sexbutton.Name = "Sexbutton";
+            this.Sexbutton.Size = new System.Drawing.Size(88, 31);
+            this.Sexbutton.TabIndex = 13;
+            this.Sexbutton.Text = "Sex";
+            this.Sexbutton.UseVisualStyleBackColor = true;
+            // 
+            // SextextBox
+            // 
+            this.SextextBox.Location = new System.Drawing.Point(590, 35);
+            this.SextextBox.Name = "SextextBox";
+            this.SextextBox.Size = new System.Drawing.Size(114, 22);
+            this.SextextBox.TabIndex = 14;
+            // 
+            // Agebutton
+            // 
+            this.Agebutton.Location = new System.Drawing.Point(496, 68);
+            this.Agebutton.Name = "Agebutton";
+            this.Agebutton.Size = new System.Drawing.Size(88, 31);
+            this.Agebutton.TabIndex = 15;
+            this.Agebutton.Text = "Age";
+            this.Agebutton.UseVisualStyleBackColor = true;
+            // 
+            // AgetextBox
+            // 
+            this.AgetextBox.Location = new System.Drawing.Point(590, 72);
+            this.AgetextBox.Name = "AgetextBox";
+            this.AgetextBox.Size = new System.Drawing.Size(114, 22);
+            this.AgetextBox.TabIndex = 16;
+            // 
+            // Datebutton
+            // 
+            this.Datebutton.Location = new System.Drawing.Point(496, 109);
+            this.Datebutton.Name = "Datebutton";
+            this.Datebutton.Size = new System.Drawing.Size(208, 31);
+            this.Datebutton.TabIndex = 17;
+            this.Datebutton.Text = "Date";
+            this.Datebutton.UseVisualStyleBackColor = true;
+            // 
+            // DatetextBox
+            // 
+            this.DatetextBox.Location = new System.Drawing.Point(496, 146);
+            this.DatetextBox.Name = "DatetextBox";
+            this.DatetextBox.Size = new System.Drawing.Size(208, 22);
+            this.DatetextBox.TabIndex = 18;
+            // 
+            // Temperaturebutton
+            // 
+            this.Temperaturebutton.Location = new System.Drawing.Point(496, 179);
+            this.Temperaturebutton.Name = "Temperaturebutton";
+            this.Temperaturebutton.Size = new System.Drawing.Size(208, 31);
+            this.Temperaturebutton.TabIndex = 19;
+            this.Temperaturebutton.Text = "Temperature";
+            this.Temperaturebutton.UseVisualStyleBackColor = true;
+            // 
+            // TemperaturetextBox
+            // 
+            this.TemperaturetextBox.Location = new System.Drawing.Point(496, 220);
+            this.TemperaturetextBox.Name = "TemperaturetextBox";
+            this.TemperaturetextBox.Size = new System.Drawing.Size(208, 22);
+            this.TemperaturetextBox.TabIndex = 20;
+            // 
+            // TimeInbutton
+            // 
+            this.TimeInbutton.Location = new System.Drawing.Point(717, 35);
+            this.TimeInbutton.Name = "TimeInbutton";
+            this.TimeInbutton.Size = new System.Drawing.Size(144, 70);
+            this.TimeInbutton.TabIndex = 21;
+            this.TimeInbutton.Text = "Time In";
+            this.TimeInbutton.UseVisualStyleBackColor = true;
+            // 
+            // TimeIntextBox
+            // 
+            this.TimeIntextBox.Location = new System.Drawing.Point(717, 114);
+            this.TimeIntextBox.Name = "TimeIntextBox";
+            this.TimeIntextBox.Size = new System.Drawing.Size(144, 22);
+            this.TimeIntextBox.TabIndex = 22;
+            // 
+            // TimeOutbutton
+            // 
+            this.TimeOutbutton.Location = new System.Drawing.Point(717, 142);
+            this.TimeOutbutton.Name = "TimeOutbutton";
+            this.TimeOutbutton.Size = new System.Drawing.Size(144, 68);
+            this.TimeOutbutton.TabIndex = 23;
+            this.TimeOutbutton.Text = "Time Out";
+            this.TimeOutbutton.UseVisualStyleBackColor = true;
+            // 
+            // TimeOuttextBox
+            // 
+            this.TimeOuttextBox.Location = new System.Drawing.Point(717, 220);
+            this.TimeOuttextBox.Name = "TimeOuttextBox";
+            this.TimeOuttextBox.Size = new System.Drawing.Size(144, 22);
+            this.TimeOuttextBox.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 622);
+            this.Controls.Add(this.TimeOuttextBox);
+            this.Controls.Add(this.TimeOutbutton);
+            this.Controls.Add(this.TimeIntextBox);
+            this.Controls.Add(this.TimeInbutton);
+            this.Controls.Add(this.TemperaturetextBox);
+            this.Controls.Add(this.Temperaturebutton);
+            this.Controls.Add(this.DatetextBox);
+            this.Controls.Add(this.Datebutton);
+            this.Controls.Add(this.AgetextBox);
+            this.Controls.Add(this.Agebutton);
+            this.Controls.Add(this.SextextBox);
+            this.Controls.Add(this.Sexbutton);
+            this.Controls.Add(this.EmailtextBox);
+            this.Controls.Add(this.Emailbutton);
+            this.Controls.Add(this.OccupationtextBox);
+            this.Controls.Add(this.Occupationbutton);
+            this.Controls.Add(this.ContactNotextBox);
+            this.Controls.Add(this.ContactNobutton);
+            this.Controls.Add(this.AddresstextBox);
+            this.Controls.Add(this.Addressbutton);
+            this.Controls.Add(this.BirthdaytextBox);
+            this.Controls.Add(this.Birthdaybutton);
+            this.Controls.Add(this.Namebutton);
+            this.Controls.Add(this.NametextBox);
+            this.Controls.Add(this.Savebutton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Savebutton;
+        private System.Windows.Forms.TextBox NametextBox;
+        private System.Windows.Forms.Button Namebutton;
+        private System.Windows.Forms.Button Birthdaybutton;
+        private System.Windows.Forms.TextBox BirthdaytextBox;
+        private System.Windows.Forms.Button Addressbutton;
+        private System.Windows.Forms.TextBox AddresstextBox;
+        private System.Windows.Forms.Button ContactNobutton;
+        private System.Windows.Forms.TextBox ContactNotextBox;
+        private System.Windows.Forms.Button Occupationbutton;
+        private System.Windows.Forms.TextBox OccupationtextBox;
+        private System.Windows.Forms.Button Emailbutton;
+        private System.Windows.Forms.TextBox EmailtextBox;
+        private System.Windows.Forms.Button Sexbutton;
+        private System.Windows.Forms.TextBox SextextBox;
+        private System.Windows.Forms.Button Agebutton;
+        private System.Windows.Forms.TextBox AgetextBox;
+        private System.Windows.Forms.Button Datebutton;
+        private System.Windows.Forms.TextBox DatetextBox;
+        private System.Windows.Forms.Button Temperaturebutton;
+        private System.Windows.Forms.TextBox TemperaturetextBox;
+        private System.Windows.Forms.Button TimeInbutton;
+        private System.Windows.Forms.TextBox TimeIntextBox;
+        private System.Windows.Forms.Button TimeOutbutton;
+        private System.Windows.Forms.TextBox TimeOuttextBox;
+    }
+}
+
