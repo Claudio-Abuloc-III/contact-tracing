5d2d6eb36233243e2ba6f94f5f7c0c66577f8c3b
Claudio-Abuloc-III <abulociiiclaudio@gmail.com>
2022-06-24 15:05:59 +0800

    Try listing all records through messagebox

--- Records.Designer.cs
+++ Records.Designer.cs
@@ -28,19 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.RecordstextBox = new System.Windows.Forms.TextBox();
+            this.Listbutton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // RecordstextBox
+            // 
+            this.RecordstextBox.Location = new System.Drawing.Point(342, 56);
+            this.RecordstextBox.Multiline = true;
+            this.RecordstextBox.Name = "RecordstextBox";
+            this.RecordstextBox.Size = new System.Drawing.Size(426, 335);
+            this.RecordstextBox.TabIndex = 0;
+            // 
+            // Listbutton
+            // 
+            this.Listbutton.Location = new System.Drawing.Point(61, 66);
+            this.Listbutton.Name = "Listbutton";
+            this.Listbutton.Size = new System.Drawing.Size(124, 40);
+            this.Listbutton.TabIndex = 1;
+            this.Listbutton.Text = "List all Records";
+            this.Listbutton.UseVisualStyleBackColor = true;
+            this.Listbutton.Click += new System.EventHandler(this.Listbutton_Click);
             // 
             // Records
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(820, 452);
+            this.Controls.Add(this.Listbutton);
+            this.Controls.Add(this.RecordstextBox);
             this.Name = "Records";
             this.Text = "Records";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox RecordstextBox;
+        private System.Windows.Forms.Button Listbutton;
     }
 }
--- Records.cs
+++ Records.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Contact_Tracing_Form
 {
@@ -15,6 +16,16 @@ namespace Contact_Tracing_Form
         public Records()
         {
             InitializeComponent();
+        }
+
+        private void Listbutton_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader(@"D:\Claudio Abuloc Files\Contact Tracing\ContactTracing.txt");
+            while (!reader.EndOfStream)
+            {
+                String line = reader.ReadToEnd();
+                MessageBox.Show(line);
+            }
         }
     }
 }
